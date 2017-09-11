f58228fb800d8067c0a28cf53c7ffaf68e9efc98
vlambs <v.lambs96@gmail.com>
2017-09-11 16:34:57 +0200

    Added : Animalerie Structure

---
+++ ZooBusiness/Animalerie/Animaux/AAnimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooBusiness.Animalerie
+{
+    abstract class AAnimal
+    {
+        public string nom { get; set; }
+        public float poids { get; set; }
+        public int age { get; set; }
+        public float taille { get; set; }
+        public string sexe { get; set; }
+        public int pattes { get; set; }
+        public int niveauFaim { get; set; }
+    }
+
+}
